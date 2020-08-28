d73f46511ac1fd7ae580033f4856567a2f71ec08
www <waclaw.ilski@gmail.com>
2020-08-28 18:57:20 +0200

    added writing/reading topics feature

--- src/main/java/com/javasurvival/spring/javasurvivalspring/MessageBoardService.java
+++ src/main/java/com/javasurvival/spring/javasurvivalspring/MessageBoardService.java
@@ -1,24 +1,24 @@
 package com.javasurvival.spring.javasurvivalspring;
 
-import io.vavr.collection.List;
 import io.vavr.collection.Map;
 import io.vavr.control.Option;
 
 public class MessageBoardService {
-    private Map<String,Topic>topics;
+    public static final String MESSAGES_JSONS = "messages.jsons";
+    private Map<String, Topic> topics;
+    private final BoardMessageWriter writer = new BoardMessageWriter(MESSAGES_JSONS);
 
     public MessageBoardService() {
-        this.topics = List.of("java","ogólny","dzowne")
-                .map(name -> Topic.createTopic(name))
-                .toMap(topic -> topic.name,topic -> topic);
+        this.topics = new BoardMessageReader().readAllTopics(MESSAGES_JSONS);
     }
 
-    public synchronized Option<Topic> getTopic(String topicName){
+    public synchronized Option<Topic> getTopic(String topicName) {
         return topics.get(topicName);
     }
 
-    public synchronized Option<Topic> addMessageToTopic(String topicName,Message message){
+    public synchronized Option<Topic> addMessageToTopic(String topicName, Message message) {
         Option<Topic> newTopic = getTopic(topicName).map(topic -> topic.addMessage(message));
+        newTopic.forEach(topic -> writer.write(topic.name, message));
         Option<Map<String, Topic>> topicsMap = newTopic.map(topic -> this.topics.put(topicName, topic));
         topicsMap.forEach(topics -> this.topics = topics);
         return newTopic;
